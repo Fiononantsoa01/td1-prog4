633dfc2c4ffa59d1be2a1b631db0e1d7784289e0
Fiononantsoa01 <andrifefyh@gmail.com>
2026-05-29 07:24:44 +0200

    fix code format

--- src/main/java/com/prog/hei/endpoint/rest/controller/health/arith/ArithController.java
+++ src/main/java/com/prog/hei/endpoint/rest/controller/health/arith/ArithController.java
@@ -1,33 +1,34 @@
 package com.prog.hei.endpoint.rest.controller.health.arith;
 
 import com.prog.hei.service.ArithService;
+import java.math.BigDecimal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.math.BigDecimal;
-
 public class ArithController {
-    private  final ArithService arithService;
-    public ArithController(ArithService arithService) {
-        this.arithService = arithService;
-    }
-    @GetMapping("/add")
-    public Long add(@RequestParam Long a, @RequestParam Long b) {
-        return arithService.addition(a, b);
-    }
+  private final ArithService arithService;
 
-    @GetMapping("/multiply")
-    public Long multi(@RequestParam Long a, @RequestParam Long b) {
-        return arithService.multiply(a, b);
-    }
+  public ArithController(ArithService arithService) {
+    this.arithService = arithService;
+  }
 
-    @GetMapping("/sub")
-    public Long sub(@RequestParam Long a, @RequestParam Long b) {
-        return arithService.substruct(a, b);
-    }
+  @GetMapping("/add")
+  public Long add(@RequestParam Long a, @RequestParam Long b) {
+    return arithService.addition(a, b);
+  }
 
-    @GetMapping("/divide")
-    public BigDecimal divide(@RequestParam Long a, @RequestParam Long b) {
-        return arithService.divide(a, b);
-    }
+  @GetMapping("/multiply")
+  public Long multi(@RequestParam Long a, @RequestParam Long b) {
+    return arithService.multiply(a, b);
+  }
+
+  @GetMapping("/sub")
+  public Long sub(@RequestParam Long a, @RequestParam Long b) {
+    return arithService.substruct(a, b);
+  }
+
+  @GetMapping("/divide")
+  public BigDecimal divide(@RequestParam Long a, @RequestParam Long b) {
+    return arithService.divide(a, b);
+  }
 }
--- src/main/java/com/prog/hei/service/ArithService.java
+++ src/main/java/com/prog/hei/service/ArithService.java
@@ -1,35 +1,32 @@
 package com.prog.hei.service;
 
-import org.springframework.stereotype.Service;
-
 import java.math.BigDecimal;
+import org.springframework.stereotype.Service;
 
 @Service
 public class ArithService {
-    public Long addition(Long a, Long b) {
-        return a + b;
+  public Long addition(Long a, Long b) {
+    return a + b;
+  }
+
+  public Long substruct(Long a, Long b) {
+    return a - b;
+  }
+
+  public Long multiply(Long a, Long b) {
+    return a * b;
+  }
+
+  public BigDecimal divide(Long a, Long b) {
+    if (b == 0) {
+      throw new ArithmeticException();
     }
 
-    public Long substruct(Long a, Long b) {
-        return a - b;
+    if (a == 0) {
+      return BigDecimal.ZERO;
     }
 
-    public Long multiply(Long a, Long b) {
-        return a * b;
-    }
-
-    public BigDecimal divide(Long a, Long b) {
-        if (b == 0) {
-            throw new ArithmeticException();
-        }
-
-        if (a == 0) {
-            return BigDecimal.ZERO;
-        }
-
-       /* return BigDecimal.valueOf(a / b);*/
-        return BigDecimal.valueOf(a)
-                .divide(BigDecimal.valueOf(b), 9, BigDecimal.ROUND_HALF_UP);
-
-    }
+    /* return BigDecimal.valueOf(a / b);*/
+    return BigDecimal.valueOf(a).divide(BigDecimal.valueOf(b), 9, BigDecimal.ROUND_HALF_UP);
+  }
 }
